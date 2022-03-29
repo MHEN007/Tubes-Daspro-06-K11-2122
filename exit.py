09db14f6108ff76e129f6c80a1fccd64d281a755
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-03-29 18:16:44 +0700

    Initial commit of exit module

---
+++ exit.py
@@ -0,0 +1,16 @@
+def exit():
+    print("Apakah Anda ingin keluar dari aplikasi ini? (Y/N)", end="")
+    confirm_exit = input()
+    exit_state = False
+    state = False
+    while state == False:
+        if confirm_exit == "Y" or confirm_exit == "y" or confirm_exit == "N" or confirm_exit == "n":
+            exit_state = True
+            state = True
+        else:
+            state = False
+            print("Apakah Anda ingin keluar dari aplikasi ini? (Y/N)", end="")
+            confirm_exit = input()
+
+#tes
+#exit()
