9da80be761391a3b7d51229188a8d0dad5aeee00
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-03-30 11:56:22 +0700

    Added condition if confirm_exit == N/n

--- exit.py
+++ exit.py
@@ -1,16 +1,18 @@
 def exit():
     print("Apakah Anda ingin keluar dari aplikasi ini? (Y/N)", end="")
     confirm_exit = input()
-    exit_state = False
+    #exit_state = False
     state = False
     while state == False:
         if confirm_exit == "Y" or confirm_exit == "y" or confirm_exit == "N" or confirm_exit == "n":
             state = True
             if confirm_exit == "Y" or confirm_exit == "y":
                 exit_state = True #klo true berarti keluar
+            else:
+                exit_state = False
         else:
             state = False
-            print("Apakah Anda ingin keluar dari aplikasi ini? (Y/N)", end="")
+            print("Apakah Anda ingin keluar dari aplikasi ini? (Y/N) ", end="")
             confirm_exit = input()
     return exit_state
 
