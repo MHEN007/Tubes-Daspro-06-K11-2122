4157b0d3c5641e447510b74ab70d2c2208b0ff94
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-04-05 14:34:12 +0700

    added \n to input

--- tambah_game.py
+++ tambah_game.py
@@ -1,7 +1,7 @@
 import isempty
 def tambah_game(nama,kategori,tahun,harga,stok):
     db_game = open("./database/game.csv","a")
-    insert = db_game.write(f"{nama};{kategori};{tahun};{harga};{stok}")
+    insert = db_game.write(f"{nama};{kategori};{tahun};{harga};{stok}\n") #ditambahkan \n agar buat baris baru
 def ulang(a,b,c,d,e):
     if isempty.isempty(a) or isempty.isempty(b) or isempty.isempty(c) or isempty.isempty(d) or isempty.isempty(e):
         return True
