d6c31bd77561c9b1a3517de918c6ef7633ece748
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-04-10 21:27:49 +0700

    Commitnya Henry

--- tambah_game.py
+++ tambah_game.py
@@ -1,9 +1,19 @@
 import isempty
 def tambah_game(nama,kategori,tahun,harga,stok,game_list):
+    count_game = 0
+    for row in game_list:
+        count_game += 1
     #db_game = open("./database/game.csv","a")
-    #insert = db_game.write(f"{nama};{kategori};{tahun};{harga};{stok}\n") #ditambahkan \n agar buat baris baru
-    game_list += [[nama, kategori, tahun, harga, stok]] #kurang satu parameter melakukan increment
+    #jumlah dari game diasumsikan maksimal 999, jadi cari kemungkinan untuk 3 digit tsb
+    if count_game==3:
+        id = "GAME" + str(count_game)
+    elif count_game==2:
+        id = "GAME0" + str(count_game)
+    else :
+        id = "GAME00" + str(count_game)
+    game_list += [[id,nama, kategori, tahun, harga, stok]] 
     return (game_list)
+
 def ulang(a,b,c,d,e):
     if isempty.isempty(a) or isempty.isempty(b) or isempty.isempty(c) or isempty.isempty(d) or isempty.isempty(e):
         return True
