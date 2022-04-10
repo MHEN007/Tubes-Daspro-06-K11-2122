1f687c80f0b805546cfd803a6aa4068ffc89c85b
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-04-10 21:30:37 +0700

    Simplified things

--- topup.py
+++ topup.py
@@ -39,5 +39,4 @@ def topup(user):
             print("Top up berhasil. Saldo "+us+ " bertambah menjadi "+str(saldo_akhir))
     else:
         print("Masukan tidak valid")
-    print(user)
     return(user)
