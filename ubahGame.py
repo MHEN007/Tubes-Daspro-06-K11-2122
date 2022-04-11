09e2c9f31bc8d743f58272ee46ffa5847561d362
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-04-11 09:58:43 +0700

    Tolong dibaca komentarnya, dibuat ulang

--- ubahGame.py
+++ ubahGame.py
@@ -1,11 +1,11 @@
-def ubah_Game(ubahGame):
+def ubah_Game(ubahGame): # <- inputnya array game (nama array cek di contoh_parse)
     cek = True
     list = 0
     count = 0
     while cek == True:
         idGame = str(input("Masukkan ID Game: "))
         for i in ubahGame:
-            if i[0] == idGame:
+            if i[0] == idGame: #ini salah, i itu bs dianggep row disini
                 cek = False
                 list = count
                 break
@@ -17,7 +17,7 @@ def ubah_Game(ubahGame):
     tahun_Rilis = str(input("Masukkan tahun rilis: "))
     hargaGame = str(input("Masukkan harga: "))
     ulang = 1
-    for i in [namaGame,kategori,tahun_Rilis,hargaGame]:
+    for i in [namaGame,kategori,tahun_Rilis,hargaGame]: #kurang ngerti ini maksudnya apa. Kan habis semua input divalidasi lgsg dimasukkin ke arranya.
         if i != '':
             ubahGame[list][ulang] = i
         ulang += 1
