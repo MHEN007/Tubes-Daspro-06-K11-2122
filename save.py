9771fe00afabc51d423842947bae0db27eaa4ac7
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-04-12 09:10:13 +0700

    Simplified save process

--- save.py
+++ save.py
@@ -20,90 +20,56 @@ def save(user, game_list, ownership, history):
     if state: #jika nama foldernya ada
         #overwrite/buat semua file yang ada di sana
         #save user.csv
-        with open(f'{parent_dir}/{folder}/user.csv', 'w') as new_user_file:
-            count = 0
-            for i in user:
-                count += 1
-
-            for i in range(count):
-                new_user_file.write(f"{user[i][0]};{user[i][1]};{user[i][2]};{user[i][3]};{user[i][4]};{user[i][5]}\n")
-        new_user_file.close()
-
+        save_file(user, "user", parent_dir, folder)
 
         #save game.csv
-        with open(f'{parent_dir}/{folder}/game.csv', 'w') as new_game_file:
-            count = 0
-            for i in game_list:
-                count += 1
-
-            for i in range(count):
-                new_game_file.write(f"{game_list[i][0]};{game_list[i][1]};{game_list[i][2]};{game_list[i][3]};{game_list[i][4]};{game_list[i][5]}\n")
-        new_game_file.close()
+        save_file(game_list, "game", parent_dir, folder)
 
         #save kepemilikan.csv
-        with open(f'{parent_dir}/{folder}/kepemilikan.csv', 'w') as new_kepemilikan_file:
-            count = 0
-            for i in ownership:
-                count += 1
-
-            for i in range(count):
-                new_kepemilikan_file.write(f"{ownership[i][0]};{ownership[i][1]}\n")
-        new_kepemilikan_file.close()
+        save_file(ownership, "kepemilikan", parent_dir, folder)
 
         #save riwayat.csv
-        with open(f'{parent_dir}/{folder}/riwayat.csv', 'w') as new_riwayat_file:
-            count = 0
-            for i in history:
-                count += 1
-
-            for i in range(count):
-                new_riwayat_file.write(f"{history[i][0]};{history[i][1]};{history[i][2]};{history[i][3]}\n")
-        new_riwayat_file.close()
+        save_file(history, "riwayat", parent_dir, folder)
         
-
     else: #jika nama folder tidak ada
         #buat folder nya
         os.mkdir(f'{parent_dir}/{folder}')
         #save user.csv
-        with open(f'{parent_dir}/{folder}/user.csv', 'w') as new_user_file:
-            count = 0
-            for i in user:
-                count += 1
-
-            for i in range(count):
-                new_user_file.write(f"{user[i][0]};{user[i][1]};{user[i][2]};{user[i][3]};{user[i][4]};{user[i][5]}\n")
-        new_user_file.close()
-
+        save_file(user, "user", parent_dir, folder)
 
         #save game.csv
-        with open(f'{parent_dir}/{folder}/game.csv', 'w') as new_game_file:
-            count = 0
-            for i in game_list:
-                count += 1
-
-            for i in range(count):
-                new_game_file.write(f"{game_list[i][0]};{game_list[i][1]};{game_list[i][2]};{game_list[i][3]};{game_list[i][4]};{game_list[i][5]}\n")
-        new_game_file.close()
+        save_file(game_list, "game", parent_dir, folder)
 
         #save kepemilikan.csv
-        with open(f'{parent_dir}/{folder}/kepemilikan.csv', 'w') as new_kepemilikan_file:
-            count = 0
-            for i in ownership:
-                count += 1
-
-            for i in range(count):
-                new_kepemilikan_file.write(f"{ownership[i][0]};{ownership[i][1]}\n")
-        new_kepemilikan_file.close()
+        save_file(ownership, "kepemilikan", parent_dir, folder)
 
         #save riwayat.csv
-        with open(f'{parent_dir}/{folder}/riwayat.csv', 'w') as new_riwayat_file:
-            count = 0
-            for i in history:
-                count += 1
-
-            for i in range(count):
-                new_riwayat_file.write(f"{history[i][0]};{history[i][1]};{history[i][2]};{history[i][3]}\n")
-        new_riwayat_file.close()
+        save_file(history, "riwayat", parent_dir, folder)
 
     #kalau sudah selesai melakukan save
     print("Data telah disimpan pada folder {}".format(folder))
+
+def save_file(data, jenis, parent_dir, folder):
+    #prosedur untuk menyimpan data
+    #count columns:
+    count_col = 0
+    for row in data:
+        for col in row:
+            count_col += 1
+        break
+    
+    #count rows:
+    count_row = 0
+    for row in data:
+        count_row += 1
+    
+    #insert data
+    with open(f'{parent_dir}/{folder}/{jenis}.csv', 'w') as save_file:
+        for i in range(count_row):
+            for j in range(count_col):
+                if j < (count_col-1):
+                    save_file.write(f"{data[i][j]};")
+                else:
+                    save_file.write(f"{data[i][j]}\n")
+    save_file.close()
+    
