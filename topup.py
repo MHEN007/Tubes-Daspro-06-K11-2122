bb846ba38a5f8935d8fcca1d477f5eed3524fe3b
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-04-12 11:13:26 +0700

    Fixed minor bugs

--- topup.py
+++ topup.py
@@ -19,6 +19,7 @@ def topup(user):
     count -=1
     '''
     found = False
+    valid = False
     count = 0
     for row in user:
         count += 1
@@ -29,15 +30,18 @@ def topup(user):
     for i in range(1,count+1):
         if user[i][1] == us:
             found = True
-            if saldo>0 or saldo+int(user[i][5])>0:
+            if saldo>0 or saldo+int(user[i][5])>=0:
                 saldo_akhir = int(user[i][5])
                 saldo_akhir += saldo
                 user[i][5] = str(saldo_akhir)
+                valid = True
                 break
             else:
-                print("Masukan tidak valid")
-    if found==False:
+                valid = False
+    if found==False and valid == False:
         print('Username "'+us+ '" tidak ditemukan.')
-    else:
+    elif found and valid:
         print("Top up berhasil. Saldo "+us+ " bertambah menjadi "+str(saldo_akhir))
+    else:
+        print("Masukan tidak valid")
     return(user)
