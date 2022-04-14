87dc0e4e5260d41267a99f75ee0d5b86867af9cc
kartinicopa <102657926+kartinicopa@users.noreply.github.com>
2022-04-14 09:59:45 +0900

    Update load.py

--- load.py
+++ load.py
@@ -9,10 +9,10 @@ def cekfolder(x):
 
 def load():
     print("loading...")
-    parser = argparse.ArgumentParser(usage="python program_binomo.py <nama_folder>") 
+    parser = argparse.ArgumentParser(usage = "python program_binomo.py <nama_folder>") 
     parser.add_argument("x") #nama untuk argumen
 
-    if isEmpty(sys.argv)==1: #cek nama folder
+    if isEmpty(sys.argv) == 1: #cek nama folder
         print("Tidak ada nama folder yang diberikan!")
         sys.exit(1)
     args=parser.parse_args() 
