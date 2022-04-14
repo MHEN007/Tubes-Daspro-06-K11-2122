e96c24f749951cfc0273f9a5eb1915c5890d1096
kartinicopa <102657926+kartinicopa@users.noreply.github.com>
2022-04-14 09:58:46 +0900

    Create load.py

---
+++ load.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import argparse
+
+#fungsi untuk cek folder
+def cekfolder(x):
+    exist = os.path.exists(x)
+    return exist
+
+def load():
+    print("loading...")
+    parser = argparse.ArgumentParser(usage="python program_binomo.py <nama_folder>") 
+    parser.add_argument("x") #nama untuk argumen
+
+    if isEmpty(sys.argv)==1: #cek nama folder
+        print("Tidak ada nama folder yang diberikan!")
+        sys.exit(1)
+    args=parser.parse_args() 
+    
+    if cekfolder(args.x): # validasi folder
+        db_user = open(f"{args.x}/user.csv")  
+        db_game = open(f"{args.x}/game.csv")
+        db_riwayat = open(f"{args.x}/riwayat.csv")
+        db_kepemilikan = open(f"{args.x}/kepemilikan.csv")
+        print("Selamat datang di antarmuka “Binomo”") 
+    else:
+        print(f"Folder “{args.x}” tidak ditemukan.")
+    return (db_user.readlines()),db_game.readlines()(db_kepemilikan.readlines())(db_riwayat.readlines())
