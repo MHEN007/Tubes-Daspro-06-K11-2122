239c6673231be89b1e275a12b3b82ca8911fcabc
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-04-23 18:01:26 +0700

    Fixed validations

--- register.py
+++ register.py
@@ -3,13 +3,22 @@ def register(user):
     nama=input("Masukan nama: ")
     username=input("Masukan username: ")
     password=input("Masukan password: ")    
+    state = False
+    while state == False:
+        if nama == "" or username == "" or password == "":
+            print("Harap mengisi semua data!")
+            nama=input("Masukan nama: ")
+            username=input("Masukan username: ")
+            password=input("Masukan password: ")
+        else: #semua terisi    
+            state = True
     count_user = 0
-    unique = False
+    unique = True
     for row in user:
         count_user += 1
         for i in range(count_user):
-            if (username != user[i][1]):
-               unique = True 
+            if (username == user[i][1]):
+               unique = False 
 
     if unique: #jika username unik
         user += [[count_user, username, nama, password, "user", "0"]] #Masukkan ke user dg role user dan saldo awal 0
