b88a0f597e0f3f0268065b6be1905f54be8db96e
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-04-24 17:21:34 +0700

    Fix database berantakan

--- listing.py
+++ listing.py
@@ -2,11 +2,22 @@ import isempty
 
 def listing (game_list) :
     
-    a = 0
-    
-    for row in game_list:
+    a = 0 #baris
+    b = 0 #kolom
+    for row in game_list: #hitung baris dalam game_list
         a += 1
     
+    for row in game_list: #hitung kolom dalam game_list
+        for col in row:
+            b += 1
+        break
+
+    copy_game_list = [["" for j in range(b)] for i in range(a)]
+    
+    for i in range(a):
+        for j in range(b):
+            copy_game_list[i][j] = game_list[i][j]
+
     sorting = input("Skema sorting: ")
     
     if ((sorting == "tahun+") or (sorting == "tahun-") or (sorting == "harga+") or (sorting == "harga-") or (sorting == "")) : 
@@ -15,10 +26,10 @@ def listing (game_list) :
             while (i < a) :
                 j = i + 1
                 while (j < a) :
-                    if game_list[i][3] > game_list[j][3] :
-                        temp = game_list[i]
-                        game_list[i] = game_list[j]
-                        game_list[j] = temp
+                    if copy_game_list[i][3] > copy_game_list[j][3] :
+                        temp = copy_game_list[i]
+                        copy_game_list[i] = copy_game_list[j]
+                        copy_game_list[j] = temp
                     j += 1
                 i += 1
             
@@ -27,10 +38,10 @@ def listing (game_list) :
             while (i < a) :
                 j = i + 1
                 while (j < a) :
-                    if game_list[i][3] < game_list[j][3] :
-                        temp = game_list[i]
-                        game_list[i] = game_list[j]
-                        game_list[j] = temp
+                    if copy_game_list[i][3] < copy_game_list[j][3] :
+                        temp = copy_game_list[i]
+                        copy_game_list[i] = copy_game_list[j]
+                        copy_game_list[j] = temp
                     j += 1
                 i += 1
         
@@ -39,10 +50,10 @@ def listing (game_list) :
             while (i < a) :
                 j = i + 1
                 while (j < a) :
-                    if game_list[i][4] > game_list[j][4] :
-                        temp = game_list[i]
-                        game_list[i] = game_list[j]
-                        game_list[j] = temp
+                    if copy_game_list[i][4] > copy_game_list[j][4] :
+                        temp = copy_game_list[i]
+                        copy_game_list[i] = copy_game_list[j]
+                        copy_game_list[j] = temp
                     j += 1
                 i += 1        
         
@@ -51,40 +62,40 @@ def listing (game_list) :
             while (i < a) :
                 j = i + 1
                 while (j < a) :
-                    if game_list[i][4] < game_list[j][4] :
-                        temp = game_list[i]
-                        game_list[i] = game_list[j]
-                        game_list[j] = temp
+                    if copy_game_list[i][4] < copy_game_list[j][4] :
+                        temp = copy_game_list[i]
+                        copy_game_list[i] = copy_game_list[j]
+                        copy_game_list[j] = temp
                     j += 1
                 i += 1        
             
         elif (sorting == "") :
-            temp = game_list
+            temp = copy_game_list
         
         #cari nama game dan harga yang paling panjang
         max_nama = 0
         max_kategori = 0
         max_harga = 0
         for i in range(0,a):
-            if isempty.lenght(game_list[i][1])>max_nama:
-                max_nama = isempty.lenght(game_list[i][1])
-            if isempty.lenght(game_list[i][2])>max_kategori:
-                max_kategori = isempty.lenght(game_list[i][2])
-            if isempty.lenght(game_list[i][4])>max_harga:
-                max_harga = isempty.lenght(game_list[i][4])            
+            if isempty.lenght(copy_game_list[i][1])>max_nama:
+                max_nama = isempty.lenght(copy_game_list[i][1])
+            if isempty.lenght(copy_game_list[i][2])>max_kategori:
+                max_kategori = isempty.lenght(copy_game_list[i][2])
+            if isempty.lenght(copy_game_list[i][4])>max_harga:
+                max_harga = isempty.lenght(copy_game_list[i][4])            
 
         #samakan panjang string dengan spasi
         for i in range(0,a):
-            if isempty.lenght(game_list[i][1])<max_nama:
-                game_list[i][1] = game_list[i][1] + (" "*(max_nama-isempty.lenght(game_list[i][1])))
-            if isempty.lenght(game_list[i][2])<max_kategori:
-                game_list[i][2] = game_list[i][2] + (" "*(max_kategori-isempty.lenght(game_list[i][2])))
-            if isempty.lenght(game_list[i][4])<max_harga:
-                game_list[i][4] = game_list[i][4] + (" "*(max_harga-isempty.lenght(game_list[i][4])))
+            if isempty.lenght(copy_game_list[i][1])<max_nama:
+                copy_game_list[i][1] = copy_game_list[i][1] + (" "*(max_nama-isempty.lenght(copy_game_list[i][1])))
+            if isempty.lenght(copy_game_list[i][2])<max_kategori:
+                copy_game_list[i][2] = copy_game_list[i][2] + (" "*(max_kategori-isempty.lenght(copy_game_list[i][2])))
+            if isempty.lenght(copy_game_list[i][4])<max_harga:
+                copy_game_list[i][4] = copy_game_list[i][4] + (" "*(max_harga-isempty.lenght(copy_game_list[i][4])))
 
         i = 1                
         while (i < a) :
-            print(str(i) + ". " + game_list[i][0] + " | " + game_list[i][1] + " | " + game_list[i][2] + " | " + game_list[i][3] + " | " + game_list[i][4] + " | " + game_list[i][5])
+            print(str(i) + ". " + copy_game_list[i][0] + " | " + copy_game_list[i][1] + " | " + copy_game_list[i][2] + " | " + copy_game_list[i][3] + " | " + copy_game_list[i][4] + " | " + copy_game_list[i][5])
             i += 1
     
     else :
